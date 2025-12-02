61e30f7ed9f7627bd940a1b08835b897e7aed341
Nuradindaa <nuradindajuniarti@gmail.com>
2025-12-02 12:33:05 +0800

    Implement getLeaderboard method in KoneksiDatabase
    Added a method to retrieve the top 10 user scores from the database.

--- src/KoneksiDatabase.java
+++ src/KoneksiDatabase.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KoneksiDatabase {
 
@@ -21,5 +23,30 @@ public class KoneksiDatabase {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, pass);
+    }
+    public static List<UserScore> getLeaderboard() {
+        List<UserScore> data = new ArrayList<>();
+        String sql = "SELECT user.username, score.wpm, score.accuracy, score.timestamp "
+                   + "FROM scores score "
+                   + "JOIN users user ON score.user_id = user.user_id "
+                   + "ORDER BY score.wpm DESC "
+                   + "LIMIT 10";
+
+        try (Connection conn = getConnection();
+            Statement statement = conn.createStatement();
+            ResultSet hasilQuery = statement.executeQuery(sql)) {
+            while (hasilQuery.next()) {
+                data.add(new UserScore(
+                    hasilQuery.getString("username"),
+                    hasilQuery.getDouble("wpm"),
+                    hasilQuery.getDouble("accuracy"),
+                    hasilQuery.getTimestamp("timestamp")
+                ));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 }
